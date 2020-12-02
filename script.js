7451be78940f830ab31cd5a732cb6c6a6c2de6e4
prakashperam <prakashperam123@gmail.com>
2020-12-02 18:08:34 +0530

    responsive to all the screens

--- script.js
+++ script.js
@@ -20,6 +20,6 @@ $(document).ready(function(){
 
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass('active');
-        $('.menu-btn i.menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
     })
 });
